72e251598b08df54aa7587de12f5d82d6ac8b902
Cyber-Programer <sifat706401@gmail.com>
2024-03-20 00:29:21 -0400

    first commit

---
+++ Pxlcrypt.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+mybanner(){
+    echo "  __         ___          __   __       __  ___ "
+    echo " |__) | \_/ |__  |       /  ' |__) \ / |__)  |  "
+    echo " |    | / \ |___ |___    \__, |  \  |  |     |  "
+    echo ""
+    echo "           Welcome To PiXeL CrYpT               "
+    echo ""
+
+}
+
+
+condition() {
+    read -p "Do you want to hide any file in an image file? (y/n): " ans
+    if [ "$ans" = 'y' ]; then
+        
+        read -p "Enter you zip file path: " zip
+        
+        if [[ "$zip" == *.zip ]]; then
+            
+            read -p "Enter your Image file path: " img
+            
+            if [[ "$img" == *.png || "$img" == *jpg || "$img" == *jpeg || "$img" == *gif ]]; then
+
+                read -p "Enter save file name : " save
+
+                if [[ "$save" == *.png || "$save" == *jpg || "$save" == *jpeg || "$save" == *gif ]]; then
+
+                    cat $img $zip > $save
+
+                else
+                    echo "Save file name must be end with .imageExtension's"
+                fi
+
+            else
+                echo "Please enter correct image file path"
+            fi
+
+        else 
+            echo "Please enter correct zip file path"
+
+        fi
+        
+    
+    elif [ "$ans" = 'n' ]; then
+        echo "Okay, goodbye!"
+    else
+        echo "Goodbye!"
+    fi
+}
+
+mybanner
+condition
---
+++ pxlopen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mybanner(){
+    echo "  __         ___          __   __       __  ___ "
+    echo " |__) | \_/ |__  |       /  ' |__) \ / |__)  |  "
+    echo " |    | / \ |___ |___    \__, |  \  |  |     |  "
+    echo ""
+    echo "           Welcome To PiXeL CrYpT               "
+    echo ""
+
+}
+
+main(){
+
+    read -p "If you want to open/unlock a pixelCrypt file? y/n: " usr
+
+    if [ "$usr" = 'y' ]; then
+
+        read -p "Enter the pixelCrypt file path: " file
+        
+        if [[ "$file"  == *.png || "$file" == *jpg || "$file" == *jpeg || "$file" == *gif  ]]; then
+
+                unzip $file
+
+        else
+            echo "Please enter the correct file"
+
+        fi
+
+    else
+        echo "Okay, goodbye!"
+    
+    fi
+}
+
+mybanner
+main
